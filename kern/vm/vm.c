393b2684aa803473e23c75a141b3fe2420394006
Tony Dao <z5118018@ad.unsw.edu.au>
2018-06-03 02:23:55 +1000

    Changed tabs to spaces

--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -15,19 +15,19 @@ void vm_bootstrap(void)
         /* Initialise VM sub-system.  You probably want to initialise your 
            frame table here as well.
         */
-		
-		/* page table initialisation */
-		struct PTE pagetable[NUMPAGES];
-		for (int i = 0; i < NUMPAGES; i++) {
-			pagetable[i]->cached = false;
-			pagetable[i]->referenced = false;
-			pagetable[i]->modified = false;
-			pagetable[i]->read = false;
-			pagetable[i]->write = false;
-			pagetable[i]->exec = false;
-			pagetable[i]->valid = false;
-			pagetable[i]->frameno = -1;
-		}
+        
+        /* page table initialisation */
+        struct PTE pagetable[NUMPAGES];
+        for (int i = 0; i < NUMPAGES; i++) {
+            pagetable[i]->cached = false;
+            pagetable[i]->referenced = false;
+            pagetable[i]->modified = false;
+            pagetable[i]->read = false;
+            pagetable[i]->write = false;
+            pagetable[i]->exec = false;
+            pagetable[i]->valid = false;
+            pagetable[i]->frameno = -1;
+        }
 }
 
 int
